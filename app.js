554b47a30e672ba7b6e7fee3af52e17cd8178c8d
David Gomes <davidrafagomes@gmail.com>
2014-07-18 21:09:19 +0100

    Removes some logging.

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ var mongo = require('mongodb');
 var monk = require('monk');
 
 var uri = process.env.MONGOLAB_URI || 'mongodb://localhost/praxedb';
-console.log("The Mongo URI is " + uri);
 var db = monk(uri);
-console.log(db);
 
 var information = db.get('information');
 
